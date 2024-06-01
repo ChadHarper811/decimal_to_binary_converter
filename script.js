dc4704706fff371f2c630ad8565981d4de6cad68
Chad Harper <chadharper811@gmail.com>
2024-06-01 13:05:06 -0500

    refactored decimal to binary function

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
+const a = () => {
+    return "freeCodeCamp " + b();
+}
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+// const decimalToBinary = (input) => {
+//     const inputs = [];
+//     const quotients = [];
+//     const remainders = [];
+
+//     if (input === 0) {
+//         result.innerText = "0";
+//         return;
+//     }
+
+//     while (input > 0) {
+//         const quotient = Math.floor(input/2);
+//         const remainder = input % 2;
+//         inputs.push(input);
+//         quotients.push(quotient);
+//         remainders.push(remainder);
+//         input = quotient;
+//     }
+//     console.log("Inputs: ", inputs);
+//     console.log("Quotients: ", quotients);
+//     console.log("Remainders: ", remainders);
+
+//     result.innerText = remainders.reverse().join("");
+// }
+
 const decimalToBinary = (input) => {
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
-
+    let binary = "";
     if (input === 0) {
-        result.innerText = "0";
-        return;
+        binary = "0";
     }
-
     while (input > 0) {
-        const quotient = Math.floor(input/2);
-        const remainder = input % 2;
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-        input = quotient;
+        binary = (input % 2) + binary;
+        input = Math.floor(input/2);
     }
-    console.log("Inputs: ", inputs);
-    console.log("Quotients: ", quotients);
-    console.log("Remainders: ", remainders);
-
-    result.innerText = remainders.reverse().join("");
+    result.innerText = binary;
 }
 
 const checkUserInput = () => {
