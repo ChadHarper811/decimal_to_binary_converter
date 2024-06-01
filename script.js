9b7ce384523c24616098f169153ae5ab22fdbb65
Chad Harper <chadharper811@gmail.com>
2024-06-01 12:55:40 -0500

    finished converter, going to dry code more

--- script.js
+++ script.js
@@ -2,16 +2,43 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+    const inputs = [];
+    const quotients = [];
+    const remainders = [];
+
+    if (input === 0) {
+        result.innerText = "0";
+        return;
+    }
+
+    while (input > 0) {
+        const quotient = Math.floor(input/2);
+        const remainder = input % 2;
+        inputs.push(input);
+        quotients.push(quotient);
+        remainders.push(remainder);
+        input = quotient;
+    }
+    console.log("Inputs: ", inputs);
+    console.log("Quotients: ", quotients);
+    console.log("Remainders: ", remainders);
+
+    result.innerText = remainders.reverse().join("");
+}
+
 const checkUserInput = () => {
-	if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
-		alert("Please provide a decimal number greater than or equal to 0");
-	}
-	console.log(numberInput.value)
+    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
+        alert("Please provide a decimal number greater than or equal to 0");
+        return;
+    }
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
 }
 
 convertBtn.addEventListener("click", checkUserInput);
 numberInput.addEventListener("keydown", (e) => {
-	if (e.key === "Enter") {
-		checkUserInput()
-	}
+    if (e.key === "Enter") {
+        checkUserInput()
+    }
 });
