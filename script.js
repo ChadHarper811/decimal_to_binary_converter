cca8afa9527b47064f0964bee2f4af1718a76f16
Chad Harper <chadharper811@gmail.com>
2024-06-01 15:22:31 -0500

    added variables to add animation to show recursive function in action

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
+const animationData = [
+    {
+        inputVal: 5,
+        marginTop: 300,
+        addElDelay: 1000
+    },
+    {
+        inputVal: 2,
+        marginTop: -200,
+        addElDelay: 1500
+    },
+    {
+        inputVal: 1,
+        marginTop: -200,
+        addElDelay: 2000
+    }
+];
 
 // First version with while loop
 // const decimalToBinary = (input) => {
@@ -48,16 +66,28 @@ const decimalToBinary = (input) => {
     } else {
         return decimalToBinary(Math.floor(input/2)) + (input % 2);
     }
-}
+};
+
+const showAnimation = () => {
+    result.innerText = "Call Stack Animation";
+    animationData.forEach((obj) => {
+        setTimeout(() => {animationContainer.innerHTML += `<p id=""></p>`}, obj.addElDelay);
+    });
+};
 
 const checkUserInput = () => {
-    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
+    const inputInt = parseInt(numberInput.value);
+    if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
         alert("Please provide a decimal number greater than or equal to 0");
         return;
     }
-    result.textContent = decimalToBinary(parseInt(numberInput.value));
+    if (inputInt === 5) {
+        showAnimation();
+        return;
+    }
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = "";
-}
+};
 
 convertBtn.addEventListener("click", checkUserInput);
 numberInput.addEventListener("keydown", (e) => {
