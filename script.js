ff491c0690c7f46bdf9c8f81663e9060ef12dc91
Chad Harper <chadharper811@gmail.com>
2024-06-01 12:25:00 -0500

    added function to check input and alert when invalid

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-	if (numberInput.value === "") {
-		
+	if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0) {
+		alert("Please provide a decimal number greater than or equal to 0");
 	}
 	console.log(numberInput.value)
 }
